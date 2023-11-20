f1036e38ed2da56b05b8261f81266fa5323bbeea
Asanalii <asanaliiospannn@gmail.com>
2023-11-20 16:25:42 +0600

    fix: changed the request for db and added route

--- internal/postgre.go
+++ internal/postgre.go
@@ -3,16 +3,19 @@ package internal
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-
 func ConnectDB() (*pgxpool.Pool, error) {
 	connURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
 	pool, err := pgxpool.New(context.Background(), connURL)
-	
+	if err != nil {
+		log.Printf("Failed to connect to database: %v", err)
+		return nil, err
+	}
 	return pool, err
 }
--- internal/router.go
+++ internal/router.go
@@ -2,18 +2,21 @@ package internal
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func NewRouter(pool *pgxpool.Pool) *chi.Mux {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	
-    r.Post("/tasks", AddTaskHandler(pool))
-    r.Get("/tasks", GetTasksHandler(pool))
-    r.Put("/tasks/{taskID}", UpdateTaskHandler(pool))
-    r.Delete("/tasks/{taskID}", DeleteTaskHandler(pool))
 
-    return r
+	controller := NewTaskController(pool)
+	handler := NewTaskHandler(controller)
+
+	r.Post("/tasks", handler.AddTaskHandler(pool))
+	r.Get("/tasks", handler.GetTasksHandler(pool))
+	r.Put("/tasks/{taskID}", handler.UpdateTaskHandler(pool))
+	r.Delete("/tasks/{taskID}", handler.DeleteTaskHandler(pool))
+
+	return r
 }
